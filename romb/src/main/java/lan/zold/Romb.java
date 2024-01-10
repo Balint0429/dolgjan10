f8a5186d843f7168557051b7f418a2b2b1f1ad4c
Eniko <you@example.com>
2024-01-10 11:43:18 +0100

    Teszt3

--- romb/src/main/java/lan/zold/Romb.java
+++ romb/src/main/java/lan/zold/Romb.java
@@ -1,10 +1,15 @@
 package lan.zold;
 
+import java.util.InputMismatchException;
+
 public class Romb {
     public double calcArea(double side, double angle) {
+        if (side <= 0 || angle <= 0) {
+            throw new InputMismatchException();
+        }
         double radians = Math.toRadians(angle);
         double area = Math.pow(side, 2) * Math.sin(radians);
 
-        return 0.1;
+        return area;
     }
 }
--- romb/src/test/java/RombTest.java
+++ romb/src/test/java/RombTest.java
@@ -14,9 +14,20 @@ public class RombTest {
     @Test
     public void testCalcArea_30_35() {
         double actual = this.romb.calcArea(30.2,35); 
-        double expected = 0.1;
+        double expected = 523.12;
         Assert.assertEquals(actual, expected, 8.6);
     }
-
+    @Test
+    public void testCalcArea_50_60() {
+        double actual = this.romb.calcArea(50, 60);
+        double expected = 2165.06;
+        Assert.assertEquals(actual, expected, 19.1);
+    }
+    @Test(expectedExceptions = InputMismatchException.class)
+    public void testCalcArea_Negative_Side() {
+        this.romb.calcArea(-1, 90);
+    }
+    
+    
     
 }
